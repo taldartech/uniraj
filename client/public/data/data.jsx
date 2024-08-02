036e0f2944b3132749a52d8e5cf760c66b0af1c2
ajay-tanwar <ajaytanwar256@gmail.com>
2024-08-02 12:44:56 +0530

    Dynamic

---
+++ client/public/data/data.jsx
@@ -0,0 +1,261 @@
+export const data = [
+  {
+    id: 1,
+    slug: "genesis-evolution",
+    title: "GENESIS & EVOLUTION: UNIVERSITY OF RAJASTHAN",
+    subTitle: "GENESIS & EVOLUTION",
+    content: `<div class="cards">
+              <p>
+                On the 8th of January 1947, the University of Rajputana appeared
+                like a little star on the educational horizon of pre-independent
+                India. That little star, now called the University of Rajasthan
+                is shining "like a diamond in the sky." Having acquired the
+                status of University with Potential for Excellence, today, the
+                University celebrates its 68th Foundation Day. Over the years
+                the University has redefined itself keeping in mind the changing
+                trends of the educational system.
+              </p>
+            </div>
+            <div class="list-card">
+              <p>
+                But let us travel down memory lane and recall how this
+                University was set up. The efforts for a separate University for
+                Rajputana State were initiated as early as 1921 when the Act for
+                the Allahabad University was passed. The Act prescribed for
+                separation of Intermediate classes from the Degree classes as a
+                condition for affiliation, which meant more expenditure for the
+                then colleges at Jaipur, Jodhpur and Ajmer, the only colleges
+                then imparting University education and affiliated to Allahabad
+                University. The location of the University became a
+                controversial issue. The question was considered in all its
+                aspects at a meeting called by the Agent to the Governor General
+                in Rajputana and attended by the ruling Princes and Chiefs at
+                Mt. Abu on 20- June 1924. It was finally decided that 'the
+                general sense of the Conference seems to be that there are
+                strong obstacles at present to a Rajputana University, and that
+                the scheme is premature but that the respective advantages of
+                affiliating either with Agra or with Delhi under changed
+                conditions as can be brought about should be explored.' The
+                colleges in Rajasthan (erstwhile Rajputana) were then affiliated
+                to Agra University after it was established in 1927. After a gap
+                of nearly two decades the efforts were again initiated in 1942
+                by Sir Mirza Ismail, the then Prime Minister of Jaipur, who
+                appointed J.C. Rollo as Special Education Officer for this
+                purpose. Sir Mirza Ismail and his successor in office Sir V.T.
+                Krishnamachari, took the initiative of holding talks with the
+                representatives of Udaipur, Jodhpur, Bikaner, Aiwar and Jaipur
+                in December, 1946. They agreed to the establishment of a
+                University at Jaipur and on 8- January, 1947, legislation was
+                promulgated and the colleges in the State formally federated
+                into the University of Rajputana, which came into existence in
+                July the same year. It was the last (2 1st) university that was
+                set up in pre- independent India.
+              </p>
+            </div>
+            <div class="list-card">
+              <p>
+                The University office was temporarily set up in the Kesargarh
+                Fort. The then Maharaja of Jaipur State, Sawai Man Singh
+                graciously made available to the university an extensive site of
+                over 300 acres. About two miles from the centre of the city, the
+                site was considered ideal for a University campus. With the Moti
+                Doongari castle overlooking the campus on the northern side, and
+                the Jhalana hills stretching from end to end in the east, the
+                site possessed that wild grandeur that characterised most
+                landmarks of Rajasthan.
+              </p>
+            </div>
+            <div class="list-card">
+              <p>
+                Dr. G. S. Mahajani, who was at that time Principal of Ferguson
+                College, Poona (now Pune) was appointed the first
+                Vice-Chancellor of the University. Dr. Mahajani was in many
+                respects an ideal person. He was a member of the Servants of
+                India Society. He used to walk to the University office from his
+                residence and did not maintain a car. Shri M. M. Verma was
+                appointed the first Registrar, who worked very closely with Dr.
+                Mahajani.
+              </p>
+            </div>
+            <div class="list-card">
+              <p>
+                The University was granted an annual grant of Rs.2.5 lacs by the
+                participating States of Rajputana. What is remarkable is that
+                the States made it clear that the "acceptance of the grant will
+                in no way detract from the autonomy of the University and its
+                freedom to organise its teaching and administration as to embody
+                in them the highest conception of University life." The
+                University has made a significant contribution in the field of
+                higher education. A peep into the past has many interesting
+                features to reveal its progressive and dynamic outlook since its
+                very beginning. The foundations laid for this University are
+                deep and strong as is amply borne out by the following extract
+                from the address delivered by Shri J. C. Rollo, on the 28- July
+                1947 at the meeting of the Academic Council. "This is the first
+                meeting of academic council of the University. It is of happy
+                significance that the first body to meet should be the Academic
+                Council. It helps to emphasize the fact that this University is
+                to be one in which academic considerations, academic
+                responsibility, shall always come first. This Council is the
+                academic conscience of the University; and, as the first person
+                honoured with its Chairmanship, I venture to suggest that today
+                and always the members may firmly maintain, at whatever cost,
+                the highest standards and the soundest policy from an academic
+                point of view. Finally, might I respectfully ask that from the
+                beginning every member may exercise a free and independent
+                personal judgment and may never accept the dictation of any
+                group of persons; a habit that has proved ruinous to some
+                Universities. In the University of Raj putana independence will
+                not be dangerous."
+              </p>
+            </div>
+
+            <div class="list-card">
+              <p>
+                The University Motto, "Dharmo Vishwasya Jagatah Pratishtha"
+                suggested by the Vice-Chancellor, Dr. G. S. Mahajani was
+                accepted by the Executive Council at its meeting held on 28-
+                January, 1948. "This enshrines the greatest contribution India
+                has made to world thought: and it is our earnest hope that
+                Universities in India will assist in the rediscovery of
+                spiritual values which the world needs at the present time." In
+                February, 1949, Shri V. T. Krishnamachari, the Diwan of the
+                Jaipur State and Pro Vice Chancellor of the University said in
+                his welcome speech, "The University has now been in existence
+                for over 1-1/2 years and its progress during this period has
+                been gratifying. For this the credit goes to Dr. Mahajani and
+                those who work with him. The University bodies have settled down
+                to their work and the University has begun to conduct all its
+                examinations and has also obtained recognition for its degrees."
+                We are trying for the establishment of an Engineering College in
+                Jodhpur and Agricultural College in Bikaner.
+              </p>
+            </div>
+            <div class="list-card">
+              <p>
+                Shri C. Rajagopalachari, the first Governor General of free
+                India, had a very clear idea of the importance of the
+                universities and their dignity. Speaking to the students of
+                Loyala College, Madras in August 1948, he had observed "we have
+                to maintain the highest standards for our universities. The
+                pressure of democracy may be allowed to bear on anything else,
+                but not on the standard of education. Do not compel the
+                universities to come down to you. You must go up the steps to
+                reach the universities."
+              </p>
+            </div>
+            <div class="list-card">
+              <p>
+                The foundation stone of the University Campus was laid by Shri
+                C. Rajagopalachari on Sunday, the February 20, 1949. He
+                concluded his address on the occasion with these words, "Nothing
+                can be achieved unless we learn to be good and correct and
+                self-disciplined and cultivate self-restraint and that, which is
+                called dharma in our beautiful language." The motto of the
+                University calls upon the faculty and the students to spread
+                this Dharma throughout the world, to make the planet earth a
+                better place to live. Dharma is the stabilising factor for the
+                whole world and if this new university which will, in the
+                history that will be written, be associated with free India,
+                truly a Rajputana University, is built on that foundation
+                according to its motto, nothing will prevent India from becoming
+                great. I invoke blessings of God on this new foundation." The
+                jurisdiction of this University at that time extended over the
+                entire Rajputana State with the exception of Ajmer-Merwara. In
+                the beginning, it was only an affiliating University with
+                twenty-two colleges in different parts of the state and it also
+                controlled High School and Intermediate Education. After the
+                merger of Ajmer-Merwara with the State of Rajasthan in 1956 this
+                region also came under the jurisdiction of the University of
+                Rajputana, which was named as University of Rajasthan in 1957.
+                With the establishment of a Board of Secondary Education in the
+                State in 1957 the schools were transferred to this board and the
+                University could then concentrate on development of higher
+                education and research. The University of Rajasthan subsequently
+                became a teaching-cum-affiliating University.
+              </p>
+            </div>
+            <div class="list-card">
+              <p>
+                The University owes much to the foresight, ability and sincerity
+                of the first three Vice-Chancellors of the University viz. Dr.
+                G.S. Mahajani, Dr. G.C. Chatterji and Dr. Mohan Sinha Mehta, who
+                played a crucial role in the development of the University, its
+                campus, recruitment of faculty and in laying down sound
+                traditions. Each of the three distinguished and highly dignified
+                Vice-Chancellors served the University for two successive terms,
+                thus, guiding the destiny of the University for the first
+                eighteen years, which was the formative period of the
+                University. To them the University owes the image it came to
+                acquire in the minds of the intellectual community of India.
+                They brought in different faculties, some of the finest scholars
+                who after having served in the University for many years, took
+                up high positions at the national level. The names of Prof. M.
+                V. Mathur, Prof. G. C. Pandey, Prof. Raj Krishna, Prof. Daya
+                Krishna, Miss A. G. Stock, Prof. P. N. Srivastava, Prof. R. C.
+                Mehrotra, Dr. S. Bashiruddin, Prof. Satish Chandra, Prof. S. P.
+                Verma, Prof. Iqbal Narain, Prof. R. K. Kaul, Prof. Raja
+                Chelliah, Prof. T.K.N. Unnithan are just a few names who brought
+                glory and renown to the University.
+              </p>
+            </div>`,
+  },
+  {
+    id: 2,
+    slug: "mission-vision",
+    title: "Vision & Mission",
+    subTitle: "Vision",
+    content: `<div class="cards">
+              <p>
+                Dharmovishvashya Jagat Pratistha
+              </p>
+            </div>
+            <div class="list-card">
+              <p>
+                The whole world is sustainable only by virtue of Satya, Ahimsa, Tyag, Pragna, Karuna, and Maitri
+              </p>
+            </div>
+            <div class="list-card">
+              <p>
+                Promotion of excellence in education and research
+by inculcating independent critical thought and scientific temper.
+              </p>
+            </div>
+            <div class="list-card">
+              <p>
+                Promotion
+of interdisciplinary research on convergence of science and technologies.
+              </p>
+            </div>
+            <div class="list-card">
+              <p>
+                Pursuit of high quality research pertaining to
+economy, social development, languages, arts, culture, energy and environment.
+              </p>
+            </div>
+
+            <div class="list-card">
+              <p>
+                Establishment and expansion of academic links and
+research collaborations with national and international institutions.
+              </p>
+            </div>
+            <div class="list-card">
+              <p>
+                Creation and strengthening of infrastructure for global competence.
+              </p>
+            </div>
+            <div class="list-card">
+              <p>
+                Critical analysis of existing social hierarchies and construction of new models of sustainable
+development, equity and gender justice.
+              </p>
+            </div>
+            <div class="list-card">
+              <p>
+                Preparing Students for Careers in Science, Social
+Science, Humanities, Commerce, Management, Fine Arts and Sports, Engineering Technology.
+              </p>
+            </div>`,
+  },
+];
--- client/src/data/menu.jsx
+++ client/src/data/menu.jsx
@@ -14,6 +14,7 @@ export const menuJson = [
           {
             id: "2",
             title: "Mission & Vision",
+            link: "mission-vision"
           },
           {
             id: "3",
